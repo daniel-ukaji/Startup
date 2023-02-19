2a5e27adb9cf6bfbd28ded28fe5848b690297ea3
Daniel Ukaji <dannyukaji@gmail.com>
2023-02-19 21:24:21 +0100

    New Commit

--- components/FooterSub.tsx
+++ components/FooterSub.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function FooterSub() {
   return (
-    <section className='flex justify-center items-center text-gray-500 pb-5'>
+    <section className='flex bg-[#F9F9F9] justify-center items-center text-gray-500 pb-5'>
         <p>©2022 GroundDesk. All right reserved</p>
     </section>
   )
