9b4a73397445dcb9a0e039d27f7a96f37157a4ac
Daniel Ukaji <dannyukaji@gmail.com>
2023-02-21 15:30:40 +0100

    New Commit.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import React from 'react'
 import globe from '../images/globe.png'
+import { Player } from '@lottiefiles/react-lottie-player'
 
 function Hero() {
   return (
     <section>
-        <div className='flex max-w-6xl mx-auto justify-between '>
+        <div className='flex max-w-6xl mx-auto justify-between  space-x-20'>
             <div className='flex flex-col justify-center pt-18'>
-                <p className='text-5xl font-black max-w-lg pb-5 font-Poppins tracking-normal leading-[1.35]'>A marketplace for startups in Africa</p>
+                <p className='text-5xl font-black max-w-xl pb-5 font-Poppins tracking-normal leading-[1.35]'>A marketplace for startups in Africa</p>
                 <p className='max-w-md font-Poppins text-sm leading-relaxed'>Lorem ipsum dolor sit amet consectetur elit. Maxime mollitia, molestiae quas ve</p>
                 
                 <div className='space-x-2 mt-16 text-xs tracking-wider'>
@@ -15,8 +16,17 @@ function Hero() {
                     <a href="#" className='border pt-3 pb-3 pl-10 pr-10 rounded-lg cursor-pointer font-Poppins'>Seller</a>
                 </div>
             </div>
-            <div className='relative w-1/2 h-96 mt-32 '>
+            {/* <div className='relative w-1/2 h-96 mt-32 '>
                 <Image alt='' src={globe} layout='fill' objectFit='contain' />
+            </div> */}
+            <div>
+                <Player 
+                    src="https://lottie.host/9328bc2e-fc80-4bfd-a4d9-767513bf3102/Z4fcO3pGVf.json"
+                    className='player'
+                    loop
+                    autoplay 
+                    speed={5}
+                />
             </div>
         </div>
     </section>
